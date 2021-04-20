b68188f62a31bd0b2464bd3ec13bf3fef46a9961
Vincent <23420208+vincentneo@users.noreply.github.com>
2021-04-20 11:36:39 +0800

    displays all day1-day5 now

--- app/src/main/java/sg/edu/rp/c347/id19007966/demoimageview/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c347/id19007966/demoimageview/MainActivity.java
@@ -3,12 +3,23 @@ package sg.edu.rp.c347.id19007966.demoimageview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView ivDay2, ivDay3, ivDay4, ivDay5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ivDay2 = findViewById(R.id.imageView2);
+        ivDay2.setImageResource(R.drawable.day2);
+        ivDay3 = findViewById(R.id.imageView3);
+        ivDay3.setImageResource(R.drawable.day3);
+        ivDay4 = findViewById(R.id.imageView4);
+        ivDay4.setImageResource(R.drawable.day4);
+        ivDay5 = findViewById(R.id.imageView5);
+        ivDay5.setImageResource(R.drawable.day5);
     }
 }
